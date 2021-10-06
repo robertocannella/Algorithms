facbb77658ea6c401595d1b1c201e9f5228d9f13
robertocannella <robertocannella@gmail.com>
2021-10-06 16:38:36 -0400

    Added diagonalDifference() to WarmUp class

--- src/com/robertocannella/Main.java
+++ src/com/robertocannella/Main.java
@@ -11,11 +11,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] arr1 = {1,2,3};
-        int[] arr2 = {3,2,1};
+        int[] arr1 = {-1,2,1};
+        int[] arr2 = {4,1,6};
+        int[] arr3 = {1,8,-1};
         List<Integer> list1 = Arrays.stream(arr1).boxed().collect(Collectors.toList());
         List<Integer> list2 = Arrays.stream(arr2).boxed().collect(Collectors.toList());
-        System.out.println(WarmUp.compareTriplets(list1, list2));
+        List<Integer> list3 = Arrays.stream(arr3).boxed().collect(Collectors.toList());
+        List<List<Integer>> matrix = new ArrayList<>(Arrays.asList(list1,list2,list3));
+
+
+        System.out.println(WarmUp.diagonalDifference(matrix));
 
     }
 }
--- src/com/robertocannella/WarmUp.java
+++ src/com/robertocannella/WarmUp.java
@@ -1,5 +1,6 @@
 package com.robertocannella;
 // HackerRank exercises
+import javax.imageio.ImageTranscoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +11,16 @@ import static java.util.stream.Collectors.toList;
 
 public class WarmUp {
 
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        // the matrix can be any size.
+        int sumUp = 0;
+        int sumDn = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            sumUp = (arr.get(0).get(2)+arr.get(1).get(1)+arr.get(2).get(0));
+            sumDn = (arr.get(0).get(0)+arr.get(1).get(1)+arr.get(2).get(2));
+        }
+        return Math.abs(sumUp - sumDn);
+    }
     public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
         List<Integer> results = new ArrayList<>(Arrays.asList(0,0));
 
@@ -24,7 +35,6 @@ public class WarmUp {
         }
         return results;
     }
-
     public static int simpleArraySum(List<Integer> ar) {
         var sum = 0;
         for(var item: ar)
