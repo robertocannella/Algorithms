00728b255f9f1b84c634d3e7de7cc5c9f465555a
robertocannella <robertocannella@gmail.com>
2021-10-09 15:17:25 -0400

    Added Implementation class.

---
+++ src/com/robertocannella/Implementation.java
@@ -0,0 +1,10 @@
+package com.robertocannella;
+
+import java.util.List;
+
+public class Implementation {
+    public static void countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
+        // Write your code here
+
+    }
+}
--- src/com/robertocannella/Main.java
+++ src/com/robertocannella/Main.java
@@ -1,8 +1,6 @@
 package com.robertocannella;
 
-import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
@@ -10,21 +8,6 @@ public class Main {
 
     public static void main(String[] args)  {
 
-        List<Integer> integers = new ArrayList<>();
-        List<Long> longs = new ArrayList<>();
 
-        Random r=new Random();
-        //generate random list
-        int size = 10;
-
-        for (int j = 0; j < size; j++) {
-            integers.add(r.nextInt());
-        }
-        for(int number: integers)
-            longs.add((long) number);
-
-        var item = WarmUp.aVeryBigSum(longs);
-        System.out.println(longs);
-        System.out.println(item);
     }
 }
