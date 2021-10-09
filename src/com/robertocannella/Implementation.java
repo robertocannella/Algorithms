fffa232b96e252b17d551ae38bf59e9568a5de23
robertocannella <robertocannella@gmail.com>
2021-10-09 15:50:06 -0400

    Added adjustedGrades class.

--- src/com/robertocannella/Implementation.java
+++ src/com/robertocannella/Implementation.java
@@ -1,10 +1,35 @@
 package com.robertocannella;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Implementation {
     public static void countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
         // Write your code here
+
+    }
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        // Write your code here
+        grades.clear();
+        grades.add(73);
+        grades.add(67);
+        grades.add(38);
+        grades.add(33);
+
+        List<Integer> adjustedGrades = new ArrayList<>();
+
+        for(var grade: grades) {
+            if (grade < 38)
+                adjustedGrades.add(grade);
+            else if (grade%5 >= 3) {
+                grade = (grade-grade%5+5);
+                adjustedGrades.add(grade);
+            }
+            else
+                adjustedGrades.add(grade);
+        }
+
+        return adjustedGrades;
 
     }
 }
--- src/com/robertocannella/Main.java
+++ src/com/robertocannella/Main.java
@@ -1,5 +1,6 @@
 package com.robertocannella;
 
+import java.sql.ClientInfoStatus;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -7,6 +8,21 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args)  {
+
+        List<Integer> list = new ArrayList<>();
+
+        Random r=new Random();
+        //generate random list
+        int size = 5;
+
+        for (int j = 0; j < size; j++)
+            list.add(r.nextInt(100));
+
+
+
+        System.out.println(list);
+        var item = Implementation.gradingStudents(list);
+        System.out.println(item);
 
 
     }
