0d140bb1367815f11a8592db3b63fb9fcc657921
robertocannella <robertocannella@gmail.com>
2021-10-10 08:26:07 -0400

    Refactored kangaroo() in Implementation class.

--- src/com/robertocannella/Main.java
+++ src/com/robertocannella/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args)  {
 
         // 0 < kangOneStartX < kangTwoStartX < 10000
-        String result = Implementation.kangaroo(2,60,100,50);
+        String result = Implementation.kangaroo(2,52,100,50);
         System.out.println(result);
     }
 }
