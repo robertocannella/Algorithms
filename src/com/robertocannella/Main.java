2485fe90ae6c4ca16fc4dcfe16bc3ba9009b8be5
robertocannella <robertocannella@gmail.com>
2021-11-04 17:06:36 -0400

    Added calculate() to Main class. HackerRank Loop exercise implemented with recursion

--- src/com/robertocannella/Main.java
+++ src/com/robertocannella/Main.java
@@ -1,13 +1,37 @@
 package com.robertocannella;
 
+import java.awt.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
+    public static void calculate(int a, int b, int n){
 
+        calculate(0, 1,a,b,n);
+    }
+    private static void calculate(int initResult, int t, int a, int b, int n){
+        if (n == 0)
+            return;
+
+        System.out.printf(a + (t*b) + initResult + " ");
+        initResult = ((t*b) + initResult);
+        n = n-1;
+
+        calculate(initResult,t*2, a, b, n);
+    }
     public static void main(String[] args)  {
-
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            calculate(a,b,n);
+            System.out.println();
+        }
+        in.close();
+    }
 
     }
 
-}
+
